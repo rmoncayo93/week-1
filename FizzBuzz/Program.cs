52ca3454405b35f6488cc2da84624351badf0e0e
Raul Moncayo <rmoncayo93@gmail.com>
2021-03-31 09:12:45 -0700

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+  class Program
+  {
+    //takes the place of a config file
+    static int start = 0;
+    static int end = 100;
+    static int div1 = 3;
+    static int div2 = 5;
+    static void Main(string[] args)
+    {
+      RunFizzBuzz(start, end);
+      GetBoundsFromUserInput();
+    }
+    private static void GetBoundsFromUserInput()
+    {
+      Console.WriteLine("Lower bound for range:");
+      Console.ReadLine();
+    }
+    // allow the user to give us upperBound, lowerBound (think validate)
+    // allow the user to set condition for multiple
+    // count from 0 to 100, and print each number per line
+    // if number is multiple of 3, print fizz
+    // if number is multiple of 5, print buzz
+    // if number multiple of both, print fizzbuzz
+    private static void RunFizzBuzz(int start, int end, int inc = 1)
+    {
+      // loop
+      for (int i = start; i <= end; i += inc)
+      {
+        PrintNumber(i, div1, div2);
+      }
+    }
+    private static void PrintNumber(int a, int div1, int div2)
+    {
+      // algorithm
+      if (a == 0)
+      {
+        WriteToConsole(a.ToString());
+        return;
+      }
+      if (a % (div1 * div2) == 0)
+      {
+        WriteToConsole("fizzbuzz");
+      }
+      else if (a % div1 == 0)
+      {
+        WriteToConsole("fizz");
+      }
+      else if (a % div2 == 0)
+      {
+        WriteToConsole("buzz");
+      }
+      else
+      {
+        WriteToConsole(a.ToString());
+      }
+
+    }
+    private static void WriteToConsole(string toPrint)
+    {
+      // print to screen
+      Console.WriteLine(toPrint);
+    }
+  }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿// using System;
+using sc = System.Console;
+
+namespace Revature.Week1.HelloWorld // namespace scope
+{
+  //class, struct, interface, enum only scopes available to the namespace
+
+  class Program // class scope
+  {
+    private static void Main() // method scope
+    {
+      sc.WriteLine("enter your name:");
+      // stdin to get your name
+      //   string in;
+      var input = System.Console.ReadLine(); // datatype inference
+                                             //   object fred2 = System.Console.ReadLine(); // anonymous datatype. object is not a datatype, c# will have to figure out the datatype at runtime
+                                             // stdout the print your name
+                                             //   System.Console.WriteLine("Hello " + input);
+      sc.WriteLine($"Hello my name is {input}"); // string interpolation, template string
+    }
+    private void UiPath()
+    {
+      { // block scope
+
+      }
+
+      {
+
+      }
+    }
+  }
+}
